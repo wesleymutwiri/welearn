201b82f132a37fa8319894ecfbfa82ecedd6a6e7
wesleymutwiri <wesleymochiemo@gmail.com>
2021-06-22 20:16:24 +0300

    split app into components for easier viewing

---
+++ src/components/Box.svelte
@@ -0,0 +1,57 @@
+<div class="box">
+    <div class="box-heading">
+        <h5>Intro to React</h5>
+        <p class="text-light">12 hours of video tutorials</p>
+    </div>
+    <div class="box-footer">
+        <div class="row">
+            <p>423 students</p>
+        </div>
+        <div class="row">
+            <div class="avatars">
+                <img src="https://source.unsplash.com/1600x900/?male" alt="" class="avatar" />
+                <img src="https://source.unsplash.com/1600x900/?male" alt="" class="avatar" />
+                <img src="https://source.unsplash.com/1600x900/?male" alt="" class="avatar" />
+                <img src="https://source.unsplash.com/1600x900/?male" alt="" class="avatar" />
+                <img src="https://source.unsplash.com/1600x900/?male" alt="" class="avatar" />
+            </div>
+            <div class="starred">
+                <svg viewBox="0 0 24 24" width="24" height="24" stroke="none" stroke-width="2" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+                    <polygon
+                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                    </polygon>
+                </svg>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .avatars {
+        padding-left: 25px;
+    }
+
+    .avatar {
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        margin-left: -25px;
+        border: 2px solid #fff;
+    }
+
+    .starred svg {
+        fill: #f2c94c;
+    }
+
+    .box {
+        background: #fff;
+        padding: 1rem 2rem;
+        display: flex;
+        flex-direction: column;
+        height: 250px;
+        min-width: 250px;
+        justify-content: space-between;
+        border-radius: 20px;
+        margin: 0 1rem;
+    }
+</style>
---
+++ src/components/SmallCard.svelte
@@ -0,0 +1,48 @@
+<div class="small-card">
+    <div class="small-card-image">
+        <img src="flame.svg" alt="">
+    </div>
+    <div class="small-card-text">
+        <h4>Build your first application</h4>
+        <p class="text-light">Codeigniter</p>
+    </div>
+    <div class="small-card-icon-next">
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
+            stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+            <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+    </div>
+</div>
+
+<style>
+    .small-card {
+        display: flex;
+        background: #fff;
+        padding: 1rem;
+        width: 400px;
+        gap: 20px;
+        border-radius: 30px;
+        margin: 0.5rem 0;
+        align-items: center;
+    }
+
+    .small-card-image {
+        width: 50px;
+        height: 50px;
+        background: #ff8577;
+        border-radius: 8px;
+        padding: 0.5rem;
+    }
+
+    .small-card-image img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .small-card-icon-next {
+        justify-self: flex-end;
+        margin: 0 auto;
+        /* justify-content: end; */
+        display: inline-flex;
+    }
+</style>
